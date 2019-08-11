6b6c77f5cc15fbfa6dc9fb65f0db0cd0963db1b1
Satya <satran004@gmail.com>
2019-08-11 21:16:55 +0800

    Account list list-with-balance create list-clear implemented

--- src/main/java/org/aion4j/maven/avm/mojo/AVMAccountFaucetMojo.java
+++ src/main/java/org/aion4j/maven/avm/mojo/AVMAccountFaucetMojo.java
@@ -3,17 +3,27 @@ package org.aion4j.maven.avm.mojo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
+import org.aion4j.avm.helper.cache.global.AccountCache;
+import org.aion4j.avm.helper.cache.global.GlobalCache;
+import org.aion4j.avm.helper.crypto.Account;
+import org.aion4j.avm.helper.crypto.AccountGenerator;
 import org.aion4j.avm.helper.exception.RemoteAvmCallException;
 import org.aion4j.avm.helper.faucet.FaucetService;
+import org.aion4j.avm.helper.remote.RemoteAvmAdapter;
 import org.aion4j.avm.helper.util.ConfigUtil;
+import org.aion4j.avm.helper.util.CryptoUtil;
+import org.aion4j.maven.avm.impl.DummyLog;
 import org.aion4j.maven.avm.impl.MavenLog;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.List;
 
-@Mojo(name = "account-topup", aggregator = true)
+@Mojo(name = "account", aggregator = true)
 public class AVMAccountFaucetMojo extends AVMLocalRuntimeBaseMojo {
 
     private final static String FAUCET_WEB_URL = "http://faucet-web.aion4j.org/";
@@ -51,16 +61,121 @@ public class AVMAccountFaucetMojo extends AVMLocalRuntimeBaseMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-        String account = getAddress();
-        if(account == null || account.trim().length() == 0)
-            throw new MojoExecutionException("Address can not be null. Please provide -Daddress=<address> or set the 'address' environment variable.");
+        boolean isTopUp = ConfigUtil.getAvmConfigurationBooleanProps("topup", false);
+        boolean isCreate = ConfigUtil.getAvmConfigurationBooleanProps("create", false);
+        boolean isList = ConfigUtil.getAvmConfigurationBooleanProps("list", false);
+        boolean isListClear = ConfigUtil.getAvmConfigurationBooleanProps("list-clear", false);
+        boolean isListWithBalance = ConfigUtil.getAvmConfigurationBooleanProps("list-with-balance", false);
 
-        String pk = ConfigUtil.getProperty("pk");
-        if(pk == null || pk.trim().length() == 0)
-            throw new MojoExecutionException("Private key can not be null. Please provide private key through -Dpk=<private key> option");
+        if(isListClear) { //If list ignore other commands
+            if(isTopUp || isCreate)
+            clearAccountCache();
+            return;
+        }
 
-        web3rpcUrl = resolveWeb3rpcUrl();
+        if(isList) { //If list ignore other commands
+            if(isTopUp || isCreate)
+                getLog().warn("You can not use other commands with 'list'.");
 
+            showAccountListFromCache(false);
+            return;
+        }
+
+        if(isListWithBalance) { //If list ignore other commands
+            if(isTopUp || isCreate)
+                getLog().warn("You can not use other commands with 'list-with-balance'.");
+
+            showAccountListFromCache(true);
+            return;
+        }
+
+        String address = null;
+        String pk = null;
+
+        if(isCreate) {
+            //Create a new account
+            Account accountObj = generateClientSideAccount();
+            address = accountObj.getAddress();
+            pk = accountObj.getPrivateKey();
+
+            writeAccountToCache(address, pk);
+        }
+
+        if(isTopUp) {
+            web3rpcUrl = resolveWeb3rpcUrl();
+
+            if(!isCreate) { //If only topup. Not create, get the address from -Daddress or system environment variable
+                address = getAddress();
+                if(address == null || address.trim().length() == 0)
+                    throw new MojoExecutionException("Address can not be null. Please provide -Daddress=<address> or set the 'address' environment variable.");
+
+                pk = ConfigUtil.getProperty("pk");
+                if(pk == null || pk.trim().length() == 0)
+                    throw new MojoExecutionException("Private key can not be null. Please provide private key through -Dpk=<private key> option");
+
+            }
+
+            executeTopup(address, pk);
+        }
+
+    }
+
+    private void clearAccountCache() {
+        GlobalCache globalCache = getGlobalAccountCache();
+        globalCache.clearAccountCache();
+        getLog().info("Account list was cleared successfully");
+    }
+
+    private void writeAccountToCache(String address, String pk) {
+        GlobalCache globalCache = getGlobalAccountCache();
+        AccountCache accountCache = globalCache.getAccountCache();
+        accountCache.addAccount(new Account(address, pk));
+        globalCache.setAccountCache(accountCache);
+    }
+
+    private void showAccountListFromCache(boolean showBalance) throws MojoExecutionException{
+        RemoteAvmAdapter remoteAvmAdapter = null;
+        if(showBalance) {//only initialize if show balance
+            web3rpcUrl = resolveWeb3rpcUrl();
+            remoteAvmAdapter = new RemoteAvmAdapter(web3rpcUrl, new DummyLog());
+        }
+
+        GlobalCache globalCache = getGlobalAccountCache();
+
+        AccountCache accountCache = globalCache.getAccountCache();
+        List<Account> accountList = accountCache.getAccounts();
+
+        if(accountList.size() > 0) {
+            getLog().info("Accounts :");
+            int index = 0;
+            for(Account account: accountList) {
+                getLog().info("Account #" + ++index);
+                getLog().info("    Address    : " + account.getAddress());
+                getLog().info("    Private key: " + account.getPrivateKey());
+
+                if(showBalance) { //Fetch balance for the account
+                    BigInteger balance = null;
+                    try {
+                        balance = remoteAvmAdapter.getBalance(account.getAddress());
+                        Double aionValue = CryptoUtil.convertAmpToAion(balance);
+
+                        getLog().info(String.format("    Balance    : %s nAmp (%s Aion)", balance, String.format("%.12f",aionValue)));
+                    }catch (Exception e) {
+                        getLog().debug("Unable to fetch balance for account: " + account.getAddress(), e);
+                        balance = BigInteger.ZERO;
+                    }
+                }
+            }
+        } else {
+            getLog().info("No account to show");
+        }
+    }
+
+    private GlobalCache getGlobalAccountCache() {
+        return new GlobalCache(getAccountCacheFolder(), MavenLog.getLog(getLog()));
+    }
+
+    private void executeTopup(String account, String pk) throws MojoExecutionException {
         getLog().info("\n");
         getLog().info("Start AION topup for address : " + account);
         getLog().info("##############################################################################################################################");
@@ -79,7 +194,47 @@ public class AVMAccountFaucetMojo extends AVMLocalRuntimeBaseMojo {
            // getLog().error("Account topup failed", e);
             throw new MojoExecutionException("Account topup failed", e);
         }
+    }
 
+    private Account generateClientSideAccount() {
+        Account account = AccountGenerator.newAddress();
+
+        String address = account.getAddress();
+
+        if(!address.startsWith("0x"))
+            address = "0x" + address;
+
+        getLog().info(String.format("Account creation successful"));
+        getLog().info("Address : " + address);
+        getLog().info("Private Key: " + account.getPrivateKey());
+
+        return account;
+    }
+
+    private String getAccountCacheFolder() {
+        String home = System.getProperty("user.home");
+
+        File cacheFolder = new File(home);
+
+        if(!cacheFolder.canWrite()) {
+            //Let's find temp folder
+            String temp = System.getProperty("java.io.tmpdir");
+            File tempFolder = new File(temp);
+
+            if(!tempFolder.canWrite()) {
+                getLog().warn("Unable to find a writable folder to keep account list cache");
+                return null;
+            } else {
+                cacheFolder = tempFolder;
+            }
+        }
+
+        File aion4jFolder = new File(cacheFolder, ".aion4j");
+        if(!aion4jFolder.exists()) {
+            aion4jFolder.mkdirs();
+        }
+
+        return aion4jFolder.getAbsolutePath();
     }
 
     @Override
@@ -91,6 +246,5 @@ public class AVMAccountFaucetMojo extends AVMLocalRuntimeBaseMojo {
     protected void executeLocalAvm(ClassLoader avmClassloader, Object localAvmInstance) throws MojoExecutionException {
 
     }
-
 
 }
